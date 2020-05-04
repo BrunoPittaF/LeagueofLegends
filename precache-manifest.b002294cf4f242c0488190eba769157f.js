bddc30416a5e1c44daaa35631176b297556c6125
BrunoPittaF <caiobrunopittaf@gmail.com>
2020-05-04 19:00:57 -0300

    Updates

---
+++ precache-manifest.b002294cf4f242c0488190eba769157f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953be6307397f4ea60267ed94585fcb1",
+    "url": "/BrunoPittaF/LeagueofLegends/index.html"
+  },
+  {
+    "revision": "53a5dbcfa2999b932f06",
+    "url": "/BrunoPittaF/LeagueofLegends/static/js/2.81e86028.chunk.js"
+  },
+  {
+    "revision": "21b9744c00ab17da64a5",
+    "url": "/BrunoPittaF/LeagueofLegends/static/js/main.b297a6c2.chunk.js"
+  },
+  {
+    "revision": "79a24f33e10096f6ae64",
+    "url": "/BrunoPittaF/LeagueofLegends/static/js/runtime-main.e47c5bf3.js"
+  },
+  {
+    "revision": "5742ab2318468d569970e38ba60536f4",
+    "url": "/BrunoPittaF/LeagueofLegends/static/media/logo.5742ab23.svg"
+  },
+  {
+    "revision": "60785311a0c4e2883cea9099185243e8",
+    "url": "/BrunoPittaF/LeagueofLegends/static/media/lol.60785311.jpg"
+  }
+]);
